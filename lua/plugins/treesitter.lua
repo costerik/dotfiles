27062d23d0fc1fabeb5bfa1bdc770bcfb5d6d2d9
Erik Ahumada <costerik@hotmail.com>
2025-09-05 18:13:27 -0500

    tree sitter updates
    angular templates config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,19 +4,34 @@ return {
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
+    "dlvandenberg/tree-sitter-angular",
   },
   config = function()
     local treesitter = require("nvim-treesitter.configs")
 
     treesitter.setup({
-      highlight = {
-        enable = true,
+      ensure_installed = {
+        "typescript",
+        "html",
+        "css",
+        "scss",
+        "javascript",
+        "angular",
       },
-      indent = {
-        enable = true,
-      },
+      highlight = { enable = true },
+      indent = { enable = true },
+
       -- enable autotagging (w/nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
       auto_install = true,
+
+      -- Add these so Lua LS shuts up
+      modules = {}, -- default empty
+      sync_install = false, -- default
+      ignore_install = {}, -- default
+
       incremental_selection = {
         enable = true,
         keymaps = {
@@ -28,6 +43,16 @@ return {
       },
     })
 
-    require("nvim-ts-autotag").setup()
+    -- Use angular parser for *.component.html but keep filetype as html
+    local ts_parsers = require("nvim-treesitter.parsers")
+    local parser_config = ts_parsers.get_parser_configs()
+    parser_config.angular.used_by = { "html" }
+
+    vim.api.nvim_create_autocmd("BufRead", {
+      pattern = "*.component.html",
+      callback = function()
+        vim.treesitter.start(0, "angular")
+      end,
+    })
   end,
 }
